cc75fc57edd394e65447d0814ef8fe220ebecc75
Mahmoud El-Naggar <mahmouddnnaggar@gmail.com>
2025-11-02 17:37:56 +0200

    main slider is done

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} antialiased`}>{children}</body>
+      <body className={`${poppins.className} antialiased`}>
+        <div className="cstm-container">{children}</div>
+      </body>
     </html>
   );
 }
--- components/home/MainSlider.tsx
+++ components/home/MainSlider.tsx
@@ -3,35 +3,77 @@
 import Image from 'next/image';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import type { SwiperOptions } from 'swiper/types';
+import { Autoplay, Pagination } from 'swiper/modules';
 
-import sliderItem1 from '@/assets/images/IPhone 14.png';
-import sliderItem2 from '@/assets/images/IPhone 17.jpg';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import sliderItem1 from '@/assets/images/slider/IPhone 14.png';
+import sliderItem2 from '@/assets/images/slider/IPhone 17.jpg';
+import appleLogo from '@/assets/images/slider/Apple_gray_logo 1.svg';
+
+import Link from 'next/link';
 
 const sliderItems = [
-  { id: 1, image: sliderItem1 },
-  { id: 2, image: sliderItem2 },
+  { id: 1, image: sliderItem1, bg: 'black' },
+  { id: 2, image: sliderItem1, bg: 'black' },
+  { id: 3, image: sliderItem1, bg: 'black' },
 ];
 
 export default function MainSlider() {
   const swiperOptions: SwiperOptions = {
-    spaceBetween: 20,
+    spaceBetween: 0,
     slidesPerView: 1,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      clickable: true,
+      dynamicBullets: true,
+      bulletClass: 'swiper-pagination-bullet bg-red-700!',
+    },
+
+    modules: [Autoplay, Pagination],
   };
 
   return (
-    <Swiper {...swiperOptions}>
+    <Swiper {...swiperOptions} className="rounded-lg overflow-hidden bg-black">
       {sliderItems.map(item => (
         <SwiperSlide key={item.id}>
-          <div className="slide relative flex justify-between items-center bg-gray-500">
+          <div
+            className={`p-10 relative flex justify-between items-center bg-iphone-${
+              item.bg
+            } ${item.bg === 'black' ? 'text-white' : ''}`}
+          >
             <div className="text">
-              <h2>iPhone Series</h2>
+              <div className="text-3xl flex items-center">
+                <Image
+                  src={appleLogo.src}
+                  alt="Apple Logo"
+                  width={30}
+                  height={30}
+                />
+                <span className="ms-2 mt-2 inline-block">
+                  iPhone <span className="text-red-700 font-bold">14</span>{' '}
+                  Series
+                </span>
+              </div>
+              <p className="text-6xl font-bold leading-20">
+                Up to 10% <br /> off{' '}
+                <span className="text-red-700">Voucher</span>
+              </p>
+              <Link href="/products" className="underline block">
+                Shop Now
+              </Link>
             </div>
             <Image
               src={item.image.src}
               alt="IPhone"
               width={1000}
               height={1000}
-              className="size-[300px] object-cover"
+              className="size-[280px] object-cover"
             />
           </div>
         </SwiperSlide>
